b4d3e71b9eb51028cdf46ce4dc4e7c7e711eea8f
eugenekapylski <kapaminski@gmail.com>
2015-04-23 08:06:43 +0000

    Created view to show all messages

--- public/modules/messages/controllers/public-messages.client.controller.js
+++ public/modules/messages/controllers/public-messages.client.controller.js
@@ -1,34 +1,8 @@
 'use strict';
 
-angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
-	function($scope, $http, $location, Authentication) {
-		$scope.authentication = Authentication;
-
-		// If user is signed in then redirect back home
-		if ($scope.authentication.user) $location.path('/');
-
-		$scope.signup = function() {
-			$http.post('/auth/signup', $scope.credentials).success(function(response) {
-				// If successful we assign the response to the global user model
-				$scope.authentication.user = response;
-
-				// And redirect to the index page
-				$location.path('/');
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
-		};
-
-		$scope.signin = function() {
-			$http.post('/auth/signin', $scope.credentials).success(function(response) {
-				// If successful we assign the response to the global user model
-				$scope.authentication.user = response;
-
-				// And redirect to the index page
-				$location.path('/');
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
-		};
+angular.module('messages').controller('PublicMessagesController', ['$scope', 
+	function($scope) {
+		$scope.allMessages = [{message: "message 1", postDate: new Date("2015-04-11T10:21:00")},
+		{message: "message 2", postDate: new Date("2015-04-11T11:26:00")}];
 	}
 ]);
